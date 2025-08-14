2aae3b2cc5dc3ebf5408a3ef91160692aa4361e7
Yazid Benmchich <benmchichyazid2006@gmail.com>
2025-08-14 23:58:59 +0100

    modified:   dataloader.h

--- dataloader.h
+++ dataloader.h
@@ -9,18 +9,14 @@ namespace NAVA {
 
 class DataLoader {
 public:
-    // MNIST dataset loading
     static std::vector<MathTools::Vector> loadMNISTImages(const std::string& filename);
     static std::vector<MathTools::Vector> loadMNISTLabels(const std::string& filename);
     
-    // CSV data loading
     static std::vector<MathTools::Vector> loadCSV(const std::string& filename, bool has_header = false);
     
-    // Data preprocessing utilities
     static MathTools::Vector normalizeVector(const MathTools::Vector& input, double min_val = 0.0, double max_val = 1.0);
     static std::vector<MathTools::Vector> normalizeDataset(const std::vector<MathTools::Vector>& dataset);
     
-    // Label encoding
     static MathTools::Vector oneHotEncode(int label, int num_classes);
     static std::vector<MathTools::Vector> encodeLabels(const std::vector<int>& labels, int num_classes);
     
